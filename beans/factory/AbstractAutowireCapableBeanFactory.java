bc8c7501ddf49fd86997717689eaad6753a58be5
whigahui <61858313+whigahui@users.noreply.github.com>
2021-04-13 10:41:55 +0800

    Create AbstractAutowireCapableBeanFactory.java

---
+++ beans/factory/AbstractAutowireCapableBeanFactory.java
@@ -0,0 +1,14 @@
+package beans.factory;
+
+/**
+ * @author whig
+ * @date 2021/4/12 21:34
+ * @desc     getBean主要由AbstractBeanFactory、AbstractAutowireCapableBeanFactory、以及DefaultListableBeanFactory实现
+ *           AbstractBeanFactory 实现了依赖关系处理
+ *           AbstractAutowireCapableBeanFactory 实现了bean的create过程
+ *           DefaultListableBeanFactory 实现了BeanDefinition的管理
+ */
+public class AbstractAutowireCapableBeanFactory {
+
+
+}
