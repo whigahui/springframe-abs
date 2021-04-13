8b8bc21e1121ceb6c5745c849c9c2e757eb4f660
whigahui <61858313+whigahui@users.noreply.github.com>
2021-04-13 10:42:06 +0800

    Create AutowiredAnnotationBeanPostProcessor.java

---
+++ beans/processor/AutowiredAnnotationBeanPostProcessor.java
@@ -0,0 +1,9 @@
+package beans.processor;
+
+/**
+ * @author whig
+ * @date 2021/4/9 10:16
+ * @desc 后者是一个 bean 后置处理器，用来完成 @AutoWired 自动注入。
+ */
+public class AutowiredAnnotationBeanPostProcessor {
+}
