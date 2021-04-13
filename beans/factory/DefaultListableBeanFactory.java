64dbce145c8388f0e85bb8ffdf696e16eabca414
whigahui <61858313+whigahui@users.noreply.github.com>
2021-04-13 10:42:03 +0800

    Create DefaultListableBeanFactory.java

---
+++ beans/factory/DefaultListableBeanFactory.java
@@ -0,0 +1,11 @@
+package beans.factory;
+
+/**
+ * @author whig
+ * @date 2021/4/9 20:24
+ * @desc BeanDefinitionRegister是向容器里注册Bean  而BeanFactory是重工厂里边拿取Bean呗 在这里哈呢
+ *       在源码当中是继承 AbstractAutowireCapableBeanFactory
+ */
+public class DefaultListableBeanFactory {
+
+}
